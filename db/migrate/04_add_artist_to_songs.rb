e77ef19181ae4b3d56fbcbd05ea667a547cf9692
github username <github email address>
2018-09-07 16:04:53 -0400

    Done.

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,8 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists_and_song do |t|
+      t.integer :artist_id
+      t.integer :song_id
+    end
+  end
 end
